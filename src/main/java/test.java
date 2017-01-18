2c4d8469844794348b32fcc7527d2a58ec18d267
wuliangliang02 <wuliangliang02@meituan.com>
2017-01-18 17:47:27 +0800

    modify

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,7 +1,9 @@
-package java;
 
 /**
  * Created by doubling_ruc on 2017/1/16.
  */
 public class test {
+    public static void main(String[] args) {
+        System.out.println("ewfwefw");
+    }
 }
