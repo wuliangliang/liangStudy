f4aabbd85288bba4437893b895ad9758992070e2
wuliangliang02 <wuliangliang02@meituan.com>
2017-01-21 15:29:30 +0800

    add weibo/*.java

---
+++ src/main/java/com/study/codelife/liang/crawl/study/weibo/tets.java
@@ -0,0 +1,10 @@
+package com.study.codelife.liang.crawl.study.weibo;
+
+/**
+ * Created by doubling_ruc on 2017/1/18.
+ */
+public class tets {
+    public static void main(String[] args) {
+        System.out.println("sfsdfsdfsdfdsf");
+    }
+}
